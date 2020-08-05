45f9de7f6bd4c6fb79ac6b45666af6c9115da3f0
unknown <kelvinmuriloas@gmail.com>
2020-08-05 20:58:04 -0300

    Implementando testes do controller ListaTarefa

--- backend/back-end-lista-de-tarefas/src/test/java/com/linecode/controller/ListaTarefaControllerTest.java
+++ backend/back-end-lista-de-tarefas/src/test/java/com/linecode/controller/ListaTarefaControllerTest.java
@@ -1,32 +1,28 @@
 package com.linecode.controller;
 
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+
+import static org.hamcrest.CoreMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.HttpStatus;
-import org.json.JSONException;
-import org.json.JSONObject;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.linecode.dto.ListaTarefaDto;
 import com.linecode.form.ListaTarefaForm;
 import com.linecode.servico.AplicacaoServico;
 
@@ -34,14 +30,20 @@ import io.restassured.http.ContentType;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
 
-@WebMvcTest
+@WebMvcTest(controllers = ListaTarefaController.class)
 public class ListaTarefaControllerTest {
 
 	@Autowired
 	private ListaTarefaController listaTarefaController;
 	
+	@Autowired
+	private MockMvc mockMvc;
+	
 	@MockBean
 	private AplicacaoServico aplicacaoServico;
+	
+	Gson g = new Gson();
+
 	
 	@BeforeEach
 	private void setup() {
@@ -54,11 +56,13 @@ public class ListaTarefaControllerTest {
 		when(this.aplicacaoServico.getListaTarefa()).thenReturn(new ArrayList<>());
 		
 		given()
-			.accept(ContentType.JSON)
+			.accept("application/json")	
 		.when()
 			.get("/lista-tarefa")
 		.then()
 			.statusCode(HttpStatus.SC_OK);
+		
+
 	}
 	
 	@Test
@@ -66,18 +70,28 @@ public class ListaTarefaControllerTest {
 	}
 	
 	@Test
-	public void deveAcessarServicoCadastrarListaTarefa_QuandoCadastrarListaTarefa() {
+	public void deveAcessarServicoCadastrarListaTarefa_QuandoCadastrarListaTarefa() throws Exception {
 		ListaTarefaForm listaTarefaForm = new ListaTarefaForm();
 		listaTarefaForm.setTitulo("Titulo");
+				
+		mockMvc.perform(MockMvcRequestBuilders.post("/lista-tarefa")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+				.content(g.toJson(listaTarefaForm)))
+				.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void naoDevePermitirCadastrarListaSemTitulo_QuandoCadastrarListaTarefa() throws Exception {
+		ListaTarefaForm listaTarefaForm = new ListaTarefaForm();
 		
-		given()
-			.accept(ContentType.ANY)
-			.body(listaTarefaForm)
-		.when()
-			.post("/lista-tarefa")
-		.then()
-			.statusCode(HttpStatus.SC_OK); 
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/lista-tarefa")
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON)
+				.content(g.toJson(listaTarefaForm)))
+				.andExpect(status().isInternalServerError());
 		
-		//verify(this.aplicacaoServico, times(1)).cadastrarListaTarefa(listaTarefaForm);
+			
 	}
 }
